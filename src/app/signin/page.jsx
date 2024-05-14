90acd8a1a80757a0cfd167dbc31fadc00b58a3fb
Sikandar873964 <873964@stu.ucenmanchester.ac.uk>
2024-05-14 21:42:31 +0530

    chore: Add sign-in page with form and layout components

---
+++ src/app/signin/page.jsx
@@ -0,0 +1,20 @@
+import { ThemeToggle } from "@/components/theme-button";
+import SignInForm from "./signInForm";
+
+export const metadata = {
+  title: "Sign Up | CRM App",
+  description: "Sign up to create an account on the CRM App",
+};
+
+export default function Signin() {
+  return (
+    <div className="flex justify-center h-screen">
+      <div className="absolute top-0 right-0 p-6">
+        <ThemeToggle />
+      </div>
+      <div className="flex items-center justify-center">
+        <SignInForm />
+      </div>
+    </div>
+  );
+}
