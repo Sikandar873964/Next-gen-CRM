e470441f97d27a45a38ee8bb6540fd712f929fa6
Sikandar873964 <873964@stu.ucenmanchester.ac.uk>
2024-05-15 20:29:43 +0530

    Add cards to dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link"
+import {
+  Activity,
+  ArrowUpRight,
+  CircleUser,
+  CreditCard,
+  DollarSign,
+  Euro,
+  Menu,
+  Package2,
+  Search,
+  Users,
+} from "lucide-react"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 function page() {
   return (
-    <div>page</div>
-  )
+    <div>
+      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+        <Card x-chunk="dashboard-01-chunk-0">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Total Transactions</CardTitle>
+            <Euro className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">5</div>
+
+          </CardContent>
+        </Card>
+        <Card x-chunk="dashboard-01-chunk-1">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Total Users</CardTitle>
+            <Users className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">+2350</div>
+          </CardContent>
+        </Card>
+        <Card x-chunk="dashboard-01-chunk-2">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Sales</CardTitle>
+            <CreditCard className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">+12,234</div>
+
+          </CardContent>
+        </Card>
+        <Card x-chunk="dashboard-01-chunk-3">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Active Users</CardTitle>
+            <Activity className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">+573</div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default page;
--- src/app/dashboard/users/add/page.jsx
+++ src/app/dashboard/users/add/page.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import Link from "next/link";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
-function page() {
-  return (
-    <div>page</div>
-  )
+export default function page() {
+return (
+    <Card className="mx-auto max-w-xl">
+        <CardHeader>
+            <CardTitle className="text-xl">Add new user</CardTitle>
+            <CardDescription>
+                Enter information to create a new user account
+            </CardDescription>
+        </CardHeader>
+        <CardContent>
+            <div className="grid gap-4">
+                <div className="grid grid-cols-1 gap-4">
+                    <div className="grid gap-2">
+                        <Label htmlFor="first-name">Name</Label>
+                        <Input id="first-name" placeholder="John Doe" required />
+                    </div>
+                    <div className="grid gap-2">
+                        <Label htmlFor="phone">Phone</Label>
+                        <Input id="phone" placeholder="123-456-7890" required />
+                    </div>
+                </div>
+                <div className="grid grid-cols-1 gap-4">
+                    <div className="grid gap-2">
+                        <Label htmlFor="email">Email</Label>
+                        <Input
+                            id="email"
+                            type="email"
+                            placeholder="m@example.com"
+                            required
+                        />
+                    </div>
+                    <div className="grid gap-2">
+                        <div className="flex items-center space-x-2">
+                            <Checkbox id="is admin" />
+                            <label
+                                htmlFor="terms2"
+                                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                                Make admin
+                            </label>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <Checkbox id="is admin" />
+                            <label
+                                htmlFor="terms2"
+                                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                                Is active
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid gap-2">
+                    <Label htmlFor="password">Password</Label>
+                    <Input id="password" type="password" />
+                </div>
+                <div className="grid gap-2">
+                    <Label htmlFor="address">Address</Label>
+                    <Textarea id="address" type="text" placeholder="Type your address here." />
+                </div>
+                <Button type="submit" className="w-full">
+                    Create user account
+                </Button>
+          
+            </div>
+        </CardContent>
+    </Card>
+);
 }
-
-export default page
