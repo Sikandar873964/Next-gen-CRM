5412145acfac7ff6b120d0fb45b4c11eb81078b3
Sikandar873964 <873964@stu.ucenmanchester.ac.uk>
2024-05-15 20:01:54 +0530

    feat: Add sidebar

---
+++ src/app/dashboard/products/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <a href='/dashboard/addproduct'>Go Back to the Dashboard</a>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,110 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {
+  ContactRoundIcon,
+  Home,
+  LineChart,
+  ListTreeIcon,
+  Package,
+  ParenthesesIcon,
+  Settings,
+  ShoppingCart,
+  Table,
+  Users2,
+} from "lucide-react";
+import { usePathname } from "next/navigation";
+
+export default function Sidebar() {
+  const sidebarItems = [
+    {
+      id: 1,
+      name: "Home",
+      link: "/dashboard",
+      tooltiptext: "Home",
+      logo: <Home className="h-5 w-5" />,
+    },
+    {
+      id: 2,
+      name: "Products",
+      link: "/dashboard/products",
+      tooltiptext: "Products",
+      logo: <Package className="h-5 w-5" />,
+    },
+    {
+      id: 3,
+      name: "Customers",
+      link: "/dashboard/customers",
+      tooltiptext: "Customers",
+      logo: <Users2 className="h-5 w-5" />,
+    },
+    {
+      id: 3,
+      name: "contact",
+      link: "/dashboard/contact",
+      tooltiptext: "Contact",
+      logo: <ContactRoundIcon className="h-5 w-5" />,
+    },
+  ];
+
+  const inactiveLink =
+    "flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8";
+
+  const activeLink =
+    "flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8";
+
+  const pathname = usePathname();
+
+  return (
+    <div>
+      <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+        <nav className="flex flex-col items-center gap-4 px-2 py-4">
+          <Link
+            href="/dashboard"
+            className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-transparent text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+          >
+            <Image src="/logo.webp" width={50} height={50} alt="Konnect Inc" />
+            <span className="sr-only">Konnect Inc</span>
+          </Link>
+          {sidebarItems.map((item, index) => (
+            <Tooltip key={index}>
+              <TooltipTrigger asChild>
+                <Link
+                  href={item.link}
+                  key={item.id}
+                  className={pathname === item.link ? activeLink : inactiveLink} // Change here
+                >
+                  {item.logo}
+                  <span className="sr-only">{item.tooltiptext}</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">{item.tooltiptext}</TooltipContent>
+            </Tooltip>
+          ))}
+        </nav>
+        <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-4">
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link
+                href="#"
+                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+              >
+                <Settings className="h-5 w-5" />
+                <span className="sr-only">Settings</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+              Placeholder Settings button if in case we require any settings
+            </TooltipContent>
+          </Tooltip>
+        </nav>
+      </aside>
+    </div>
+  );
+}
