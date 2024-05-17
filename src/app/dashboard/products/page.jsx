2e0653721526a60b1c426ffe5dcf5dd6b1e534cc
Sikandar873964 <873964@stu.ucenmanchester.ac.uk>
2024-05-17 23:36:32 +0530

    feat: Update products page with table component and metadata

--- src/app/dashboard/products/page.jsx
+++ src/app/dashboard/products/page.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 import { ProductTable } from "./productTable";
+import { fetchProducts } from "@/app/lib/data";
 
 export const metadata = {
-  title: 'Products | CRM App',
+  title: "Products | CRM App",
+};
+
+async function getData() {
+  const response = await fetchProducts();
+
+  const formattedData = response.products.map((product) => ({
+    title: product.title,
+    price: product.price,
+    description: product.desc,
+    stock: product.stock,
+    createdAt: product.createdAt,
+    color: product.color,
+    id: product._id.toString(),
+  }));
+
+  return formattedData;
 }
 
-function page() {
+export default async function page() {
+  const data = await getData();
+
   return (
     <div>
       <div className="text-4xl tracking-wider font-semibold">Products</div>
       <p className="text-sm text-muted-foreground">
         Add, view, edit or remove Products from the CRM
       </p>
-      <ProductTable />
+      <ProductTable data={data} />
     </div>
   );
 }
-
-export default page;
