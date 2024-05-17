ccb5f437ebc54a8a5958c2b9bc41f944b6b63a51
Sikandar873964 <873964@stu.ucenmanchester.ac.uk>
2024-05-17 23:37:15 +0530

    chore: Update product page with form and card components

---
+++ src/app/dashboard/products/[id]/page.jsx
@@ -0,0 +1,193 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft, PoundSterlingIcon } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardDescription,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+import { fetchProduct } from "@/app/lib/data";
+import { updateProduct } from "@/app/lib/actions";
+
+export const metadata = {
+  title: "View Product | CRM App",
+};
+export default async function page({ params }) {
+  const { id } = params;
+  const product = await fetchProduct(id);
+  return (
+    <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+      <form
+        action={updateProduct}
+        className="mx-auto grid max-w-full flex-1 auto-rows-max gap-4"
+      >
+        <div className="flex items-center gap-4">
+          <Button variant="outline" size="icon" className="h-7 w-7" onclick="">
+            <ChevronLeft className="h-4 w-4" />
+            <span className="sr-only">Back</span>
+          </Button>
+          <h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
+            Update Product: {product.title}
+          </h1>
+          <div className="hidden items-center gap-2 md:ml-auto md:flex">
+            <Button variant="outline" size="sm">
+              Discard
+            </Button>
+            <Button size="sm" type="submit">
+              Update Product
+            </Button>
+          </div>
+        </div>
+        <div className="grid gap-4 md:grid-cols-[1fr_250px] lg:grid-cols-3 lg:gap-8">
+          <div className="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
+            <Card x-chunk="dashboard-07-chunk-0">
+              <CardHeader>
+                <CardTitle>Product Details</CardTitle>
+                <CardDescription>
+                  Add details about the product here
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-6">
+                  <div className="grid gap-3">
+                    <Label htmlFor="name">Name</Label>
+                    <Input
+                      id="name"
+                      type="text"
+                      className="w-full"
+                      placeholder={product.title}
+                    
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="description">Description</Label>
+                    <Textarea
+                      id="desc"
+                      placeholder={product.desc}
+                      className="min-h-32"
+                    
+                    />
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>Additional Details</CardTitle>
+                <CardDescription>
+                  Enter additional details about the product here
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <Label htmlFor="size">Size</Label>
+
+                <Input
+                  id="size"
+                  type="string"
+                  className="w-full"
+                  placeholder={product.size}
+                />
+                <div>
+                  {" "}
+                  <Label htmlFor="color">Color</Label>
+                  <Input
+                    id="color"
+                    type="string"
+                    className="w-full"
+                    placeholder={product.color}
+                  />
+                </div>
+
+                <div>
+                  <Label htmlFor="category">Category</Label>
+                  <Select>
+                    <SelectTrigger id="category" aria-label="Select category">
+                      <SelectValue placeholder="Select category" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="mobiles">Mobiles</SelectItem>
+                      <SelectItem value="computers">Computers</SelectItem>
+                      <SelectItem value="accessories">Accessories</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+          <div className="grid auto-rows-max items-start gap-4 lg:gap-8">
+            <Card x-chunk="dashboard-07-chunk-3">
+              <CardHeader>
+                <CardTitle>Product Stock</CardTitle>
+                <CardDescription>
+                  Add number of stock units here
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-6">
+                  <div className="grid gap-3">
+                    <Input
+                      id="name"
+                      type="number"
+                      className="w-full"
+                      placeholder={product.stock}
+                    
+                    />
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+            <Card className="overflow-hidden" x-chunk="dashboard-07-chunk-4">
+              <CardHeader>
+                <CardTitle>Product Image</CardTitle>
+                <CardDescription>Add a link to the image here</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Input
+                  id="image"
+                  type="text"
+                  className="w-full"
+                  placeholder="https://example.com/image.jpg"
+                />
+              </CardContent>
+            </Card>
+            <Card className="overflow-hidden">
+              <CardHeader>
+                <CardTitle>Product Price</CardTitle>
+                <CardDescription>Add a link to the image here</CardDescription>
+              </CardHeader>
+              <CardContent className="flex flex-row items-center">
+                <PoundSterlingIcon className="h-6 w-6 mr-2 text-muted-foreground" />
+                <Input
+                  id="name"
+                  type="number"
+                  className="w-full"
+                  placeholder={product.price}
+                
+                />
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+        <div className="flex items-center justify-center gap-2 md:hidden">
+          <Button variant="outline" size="sm">
+            Discard
+          </Button>
+          <Button size="sm">Save Product</Button>
+        </div>
+      </form>
+    </main>
+  );
+}
